a28f0a52921e70b4d0ece4fb0807708a18ae753e
Rémy Sanchez <remy.sanchez@hyperthese.net>
2014-01-14 02:46:15 +0100

    Well, I can still try this

--- lib/required/pip.sh
+++ lib/required/pip.sh
@@ -3,9 +3,11 @@
 function install_pip {
 	shout "Installing Pip dependencies"
 
+	echo "Installing pip" | indent
+	curl -s https://raw.github.com/pypa/pip/master/contrib/get-pip.py | python | indent
+
+	echo "Installing dependencies" | indent
 	PIP_SRC="$CACHE_DIR/pip/src"
-
 	mkdir -p "$PIP_SRC"
-
 	pip install -r "$BUILD_DIR/requirements.txt" --exists-action=w --src="$PIP_SRC" | indent
 }
